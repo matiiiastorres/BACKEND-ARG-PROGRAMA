a15860ab31e3b090a6e8c3f9c5fcbed6b8e742ad
MATIAS TORRES <matiiiastorres@gmail.com>
2022-08-19 21:30:14 -0300

    corriiendo cors

--- src/main/java/com/portfolio/mgb/Controller/CEducacion.java
+++ src/main/java/com/portfolio/mgb/Controller/CEducacion.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://arg-prog-matias-torres.web.app")
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
